03818f9d083d947410a3aa85eb1fde1d19e2692f
rchikhi <rayan.chikhi@univ-lille1.fr>
2019-02-28 18:37:08 +0100

    rayan

--- Solution.py
+++ Solution.py
@@ -5,4 +5,9 @@ class Solution(object):
     def __init__(self, arg):
         super(Solution, self).__init__()
         self.arg = arg
+
+    def save(self):
+        outfile = open("sol.txt","w")
+        f.write("test\n")
+        f.close()
         
