d7173a96a7dcdeaba0d6b115aebb69f3eca435ed
Nicolas MAILLET <nicolas.maillet@pasteur.fr>
2019-02-28 20:48:46 +0100

    zafeer

--- Slide.py
+++ Slide.py
@@ -9,7 +9,7 @@ class Slide(object):
             assert photos[0].orientation == photos[1].orientation == "V", "Both photos should be V"
             self.keywords = photos[0].keywords | photos[1].keywords
             self.id = str(photos[0].id) + " " + str(photos[1].id)
-            self.photos = list(photos[0], photos[1])
+            self.photos = (photos[0], photos[1])
         if len(photos) == 1:
             assert photos[0].orientation == "H", "Photo should be H"
             self.keywords = photos[0].keywords
@@ -23,11 +23,20 @@ class Slide(object):
 
     def switch_photos_A(self, slide):
         tmp = self.photos[0]
-        self.photos[0] = slide.photos[0]
-        slide.photos[0] = tmp
+        self.photos = (slide.photos[0], self.photos[1])
+        self.keywords = slide.photos[0].keywords | self.photos[1].keywords
+        self.id = str(slide.photos[0].id) + " " + str(self.photos[1].id)
+
+        slide.photos = (tmp, slide.photos[1])
+        slide.keywords = tmp.keywords | slide.photos[1].keywords
+        slide.id = str(tmp.id) + " " + str(slide.photos[1].id)
 
     def switch_photos_B(self, slide):
         tmp = self.photos[0]
-        self.photos[0] = slide.photos[1]
-        slide.photos[1] = tmp
+        self.photos = (slide.photos[1], self.photos[1])
+        self.keywords = slide.photos[1].keywords | self.photos[1].keywords
+        self.id = str(slide.photos[1].id) + " " + str(self.photos[1].id)
 
+        slide.photos = (slide.photos[0], tmp)
+        slide.keywords = slide.photos[0].keywords | tmp.keywords
+        slide.id = str(slide.photos[0].id) + " " + str(tmp.id)
