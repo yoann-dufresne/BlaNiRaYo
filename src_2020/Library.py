cdb9199a7f522ed2bd48594b12eda38adc451ee0
Blaise Li <blaise.li__git@nsup.org>
2020-02-20 19:42:13 +0100

    Add book mask.

--- src_2020/Library.py
+++ src_2020/Library.py
@@ -6,6 +6,13 @@ class Book:
         self.ide = ide
         self.score = score
 
+
+def mask_books(books, avoid):
+    for book in books:
+        if book not in avoid:
+            yield book
+
+
 class Library:
 
     def __init__(self, ide, signup, ship):
@@ -26,8 +33,10 @@ class Library:
     def worthy_books_first(self):
         return sorted(self.books, key=attrgetter("score"), reverse=True)
 
-    def interest1(self, time_avail):
+    def interest1(self, time_avail, avoid=set()):
         """A potential heuristic measure of library potential interest."""
         time_bookflow = time_avail - self.signup
         nb_books_scannable = time_bookflow // self.ship
-        return sum(b.score for b in self.worthy_books_first[:nb_books_scannable])
+        return sum(b.score for b in mask_books(
+            self.worthy_books_first[:nb_books_scannable],
+            avoid)
