e92937528cbb2b6b2fb63995ca02a7f4ec2c153e
Rayan <rayan.chikhi@pasteur.fr>
2020-02-20 19:20:22 +0100

    modified parser

--- src_2020/Parser.py
+++ src_2020/Parser.py
@@ -1,15 +1,19 @@
 import sys
 from src_2020.Library import Library
 
-def parse():
-    nb_books, nb_lib, nb_days = [int(x) for x in sys.stdin.readline().strip().split(' ')]
-    scores = [int(x) for x in sys.stdin.readline().strip().split(' ')]
+def parse(source=None):
+    if source is None:
+        source = std.stdin
+    else:
+        source = open(source)
+    nb_books, nb_lib, nb_days = [int(x) for x in source.readline().strip().split(' ')]
+    scores = [int(x) for x in source.readline().strip().split(' ')]
 
     libs = []
     for l in range(nb_lib):
-        lib_values = [int(x) for x in sys.stdin.readline().strip().split(' ')]
+        lib_values = [int(x) for x in source.readline().strip().split(' ')]
         lib = Library(lib_values[1], lib_values[2])
-        lib.add_books([int(x) for x in sys.stdin.readline().strip().split(' ')])
+        lib.add_books([int(x) for x in source.readline().strip().split(' ')])
         libs.append(lib)
 
     return nb_books, nb_lib, nb_days, scores, libs
