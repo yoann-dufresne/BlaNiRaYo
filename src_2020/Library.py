131c96ff7ef365d8842e4bb95d51b7e04166b936
Nicolas MAILLET <nicolas.maillet@pasteur.fr>
2020-02-20 21:41:02 +0100

    gfgs

--- src_2020/Library.py
+++ src_2020/Library.py
@@ -12,6 +12,10 @@ class Book:
     def weighted_score(self):
         return self.score / self.frq
 
+    @property
+    def inv_frq(self):
+        return 1 / self.frq
+
 
 def mask_books(books, avoid):
     return [b for b in books if b not in avoid]
@@ -45,6 +49,9 @@ class Library:
     def worthy_books_first(self, time_available):
         return sorted(self.books, key=attrgetter("weighted_score"), reverse=True)[:time_available*self.ship]
 
+    def worthy_books_first2(self, time_available):
+        return sorted(self.books, key=attrgetter("inv_frq"), reverse=True)[:time_available*self.ship]
+
     def nb_books_scannable(self, time_avail):
         time_bookflow = time_avail - self.signup
         return time_bookflow // self.ship
@@ -65,3 +72,10 @@ class Library:
 
         return sum(b.score for b in mask_books(self.worthy_books_first(time_avail-self.signup)[:nb_books_scannable], avoid))
 
+    def interest3(self, time_avail, avoid=set()):
+        """A potential heuristic measure of library potential interest."""
+        # time_bookflow = time_avail - self.signup
+        # nb_books_scannable = time_bookflow // self.ship
+        nb_books_scannable = self.nb_books_scannable(time_avail-self.signup)
+
+        return sum(1/b.frq for b in mask_books(self.worthy_books_first2(time_avail-self.signup)[:nb_books_scannable], avoid))
