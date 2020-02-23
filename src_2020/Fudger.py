95bb74ba4c0fdf7010305fe824c3c7f2defd0eb1
Rayan <rayan.chikhi@pasteur.fr>
2020-02-23 02:07:19 +0100

    fudger now replaces k libs at a time

--- src_2020/Fudger.py
+++ src_2020/Fudger.py
@@ -10,6 +10,8 @@ from src_2020.Library import Library
 from src_2020.Scorer import scorer 
 import random
 import copy
+from itertools import combinations
+random.seed(17)
 
 def solution_read(solution_file, libs):
     nb_libraries = None
@@ -42,13 +44,18 @@ def solution_score(libs,scores):
         total_score += scores[book]
     return total_score
 
-def solution_replace_lib(sol_libs, i, lib, nb_days):
+def solution_replace_lib(sol_libs, i, lib, nb_days, last_interval_index):
     # avoid all other books from the solution
     avoid = set([x for j in range(len(sol_libs)) for x in sol_libs[j].books_to_scan if j != i ])
-    time_available = nb_days - (sol_libs[i].signup_on + sol_libs[i].signup)
-    new_sol = copy.copy(sol_libs) # gotta copy
+    new_sol = copy.copy(sol_libs) # gotta have a copy 
     new_sol[i] = copy.copy(lib)
-    new_sol[i].signup_on = sol_libs[i].signup_on
+    # update new signup_on times (everything may be shifted)
+    for j in range(i,len(sol_libs)):
+        if j > 0:
+            new_sol[j].signup_on = new_sol[j-1].signup_on + new_sol[j-1].signup 
+        else:
+            new_sol[0].signup_on = 0
+    time_available = nb_days - (new_sol[i].signup_on + new_sol[i].signup)
     lib.signed = True # to avoid books_by_worth adjust time_available
     new_sol[i].books_to_scan =  [book.ide for book in lib.books_by_worth(avoid=avoid, time_available=time_available)]
     new_sol[i].signed = True 
@@ -70,23 +77,42 @@ if __name__ == "__main__":
     print("original solution, computing its score..")
     scorer(problem_file,solution_file)
 
+    tentatives = 0
     while True:
-        # randomly replace a library by another one
-        i = random.randint(0,len(sol_libs)-1)
-        other_libs_ids = list(set(range(len(libs))) - set([sol_libs[i].ide for i in range(len(sol_libs))]))
-        #nb_sol_days = sum([lib.signup for lib in sol_libs])
-        #buffer_days = nb_days - nb_sol_days # i tried to allow longerl libs, but i realized later that it shifts everything after
-        new_lib = libs[random.choice(other_libs_ids)]
-        if new_lib.signup > sol_libs[i].signup: 
-            #print("new lib has higher signup time..", sol_libs[i].signup, lib.signup)
-            # TODO could see how many signup days are actually left
-            continue
-        new_sol_libs = solution_replace_lib(sol_libs, i, new_lib, nb_days)
-        new_score = solution_score(new_sol_libs,scores)
-        if new_score > old_score:
-            print("!new best (%d)" %new_score)
-            old_score = new_score
-            sol_libs = copy.copy(new_sol_libs)
-            output(output_file, sol_libs)
-            print("confirming score..")
-            scorer(problem_file,output_file)
+        # randomly replace k libraries by other ones
+        k=1 # k>1 isn't guaranteed to be bugfree and actually doesnt seem to be that effective
+        # select an interval (start_index,start_index+k) within the solution
+        start_index = random.randint(0,len(sol_libs)-k)
+        # compute total signup time within that interval
+        total_signup = sum([sol_libs[start_index+j].signup for j in range(k)]) 
+        # see which libraries are already used in the solution
+        already_used_libs = set([sol_libs[i].ide for i in range(len(sol_libs))])
+        # get a list of possible replacements from the original library list (minus those already used)
+        # caveat: so, a possible replacement has zero intersection with libs of the existing solution
+        possible_replacements = combinations(set(range(len(libs))) - already_used_libs,k)
+        sys.stdout.write('\r attempt: %d' %tentatives)
+        sys.stdout.flush()
+        tentatives+=1
+        #possible_replacement = random.choice(list(possible_replacements))
+        #if True:
+        for possible_replacement in possible_replacements:
+            # estimate the total signup time of the replacement before proceeding
+            repl_total_signup = sum([libs[j].signup for j in possible_replacement])
+            if repl_total_signup > total_signup:
+                continue
+            #at this point we know that the new libraries won't take more time than the ones in the previous interval
+            #so let's replace the old ones with the new ones in the solution.
+            new_sol_libs = copy.copy(sol_libs)
+            for i,selected in enumerate(possible_replacement):
+                new_lib = libs[selected]
+                new_sol_libs = solution_replace_lib(new_sol_libs, start_index+i, new_lib, nb_days, start_index+k)
+            # evaluate new solution score
+            new_score = solution_score(new_sol_libs,scores)
+            if new_score > old_score:
+                print("!new best (%d)" %new_score,"by replacing",set(range(start_index,start_index+k)))
+                old_score = new_score
+                sol_libs = copy.copy(new_sol_libs)
+                output(output_file, sol_libs)
+                print("confirming score..")
+                scorer(problem_file,output_file)
+            #break
--- src_2020/Scorer.py
+++ src_2020/Scorer.py
@@ -4,15 +4,23 @@ import src_2020.Parser as Parser
 def scorer(input_file, solution_file):
     _, _, nb_days, scores, libs, books = Parser.parse(input_file)
     all_books = set()
+    libraries_used = set()
     # assumption: libs are in the order they'll output books
     day = 0
-    for i, line in enumerate(open(solution_file).read().split('\n')):
+    all_lines = open(solution_file).read().split('\n')
+    est_nb_libs = len(all_lines)-1 // 2
+    for i, line in enumerate(all_lines):
         if len(line.strip()) == 0: break
         if i == 0:
             nb_libraries = int(line)
             continue
         if i%2 == 1:
             library_id, nb_books = map(int,line.strip().split())
+
+            # safety check
+            if library_id in libraries_used:
+                print("warning! library %d used twice" % library_id)
+            libraries_used.add(library_id)
         else:
             lib_books = list(map(int,line.strip().split()))
             day += libs[library_id].signup
@@ -21,7 +29,7 @@ def scorer(input_file, solution_file):
             all_books |= set(lib_books)
             remaining_days = nb_days - day
             if len(lib_books) > remaining_days * libs[library_id].ship:
-                print("warning! library",library_id,"outputs more books than it has time for (%d > %d)" % (len(lib_books), remaining_days * libs[library_id].ship))
+                print("warning! library",library_id,"(number %d/%d in solution)" % (i//2,est_nb_libs),"with capacity",libs[library_id].ship,"outputs more books than it has time for (%d > %d)" % (len(lib_books), remaining_days * libs[library_id].ship))
 
     print(len(all_books),"books total were selected, ",remaining_days,"unused signup day(s)")
     #print(all_books)
